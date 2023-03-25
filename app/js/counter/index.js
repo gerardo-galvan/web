34013a5fabe8c93a84aefd8d86867d07f122e07d
Gerardo Galván <gerardo-galvan@users.noreply.github.com>
2023-03-25 11:44:42 -0400

    reset counter to 0 when save button is pressed

--- app/js/counter/index.js
+++ app/js/counter/index.js
@@ -1,17 +1,19 @@
 // document.getElementById("count-el").innerText = 5
 
-let countEL = document.getElementById("count-el");
+let countEl = document.getElementById("count-el");
 let saveElement = document.getElementById("save-element")
 let count  = 0;
 
  function increment() {
     count +=  1;
-    countEL.textContent = count;
+    countEl.textContent = count;
     
  }
   
  function save() {
     let countString = count + "-";
     saveElement.textContent += countString;
+    countEl.textContent = 0;
+    count = 0;
     
  }
